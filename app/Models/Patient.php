20f374f58e53b1a8c09c334550ddc643b892c8e6
asyrafprdmya <asrappramudya14@gmail.com>
2025-11-26 02:16:35 +0800

    Update Admin

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class patients extends Model
+class Patient extends Model
 {
     use HasFactory;
     
